54b0e4ba766b70a3f6acd8e6e0a1aea76275b5eb
rahulnakre <rahulnakre@gmail.com>
2020-10-20 21:23:26 -0400

    yes

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/pborman/getopt"
 )
@@ -13,6 +15,7 @@ func main() {
 	// https://cloudflare-assignment.rahulnakre.workers.dev/
 	urlFlag := getopt.StringLong("url", 0, "", "url of a site")
 	helpFlag := getopt.BoolLong("help", 0, "Help")
+	profileFlag := getopt.Int64Long("profile", 0, 1, "Profile")
 	getopt.Parse()
 
 	if *helpFlag {
@@ -20,58 +23,53 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Println("sup " + *urlFlag + " hehe")
+	// tcpAddr, err := net.ResolveTCPAddr("tcp4", "www.google.com")
+	// errHandler(err)
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", "www.google.com")
-	errHandler(err)
+	// fmt.Printf("here: %v", tcpAddr)
 
-	fmt.Printf("here: %v", tcpAddr)
+	// conn, err := net.Dial("tcp", *urlFlag)
+	// errHandler(err)
+	// defer conn.Close()
 
-	conn, err := net.Dial("tcp", *urlFlag)
-	errHandler(err)
+	// _, err = conn.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
+	// errHandler(err)
+
+	// res, err := ioutil.ReadAll(conn)
+	// errHandler(err)
+
+	// fmt.Println(string(res))
+
+	// s := "http://ran-home.s3-website-us-east-1.amazonaws.com"
+
+	u, err := url.Parse(*urlFlag)
+	checkError(err)
+	fmt.Println(u.Host)
+
+	startTime := time.Now()
+	// conn, err := net.Dial("tcp", "cloudflare-assignment.rahulnakre.workers.dev:http")
+	// conn, err := net.Dial("tcp", u.Host+":http")
+	conn, err := net.Dial("tcp", "www.google.com:http")
+	// conn, err := net.Dial("tcp", "ran-home.s3-website-us-east-1.amazonaws.com:http")
+
+	checkError(err)
 	defer conn.Close()
-
-	_, err = conn.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
-	errHandler(err)
+	size, err := fmt.Fprintf(conn, "GET / HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n", u.Host)
+	checkError(err)
+	fmt.Println(size)
 
 	res, err := ioutil.ReadAll(conn)
-	errHandler(err)
+	endTime := time.Now()
+	checkError(err)
 
-	fmt.Println(string(res))
+	fmt.Printf(string(res) + "\n")
+	fmt.Printf("%f\n", endTime.Sub(startTime).Seconds())
 
-	// buf := make([]byte, 0, 4096)
-	// recv := make([]byte, 256)
-	// for {
-	// 	fmt.Println("fere")
-	// 	size, err := conn.Read(recv)
-	// 	fmt.Printf("read size: %d", size)
-
-	// 	// errHandler(err)
-	// 	if err != nil {
-	// 		break
-	// 	}
-	// 	buf = append(buf, recv[:size]...)
-	// }
-	// fmt.Printf("%s", string(buf))
-	// conn.
-
-	// addr, err := net.LookupAddr("ispycode.com")
-	// errHandler(err)
-
-	// fmt.Println(addr)
-
-	// tcpAddr, err := net.ResolveTCPAddr("tcp", "8.8.8.8")
-	// errHandler(err)
-	// net.DialIP()
-	// fmt.Println(tcpAddr)
-	// conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	// errHandler(err)
-
-	// fmt.Printf("%v\n", conn)
+	fmt.Println(*profileFlag)
 
 }
 
-func errHandler(err error) {
+func checkError(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(0)

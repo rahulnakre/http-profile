fab6cf559e99f8ab57a4f04e356083f28a7bdff5
rahulnakre <rahulnakre@gmail.com>
2020-10-22 01:20:37 -0400

    working on not counting the header for res body length

--- main.go
+++ main.go
@@ -8,29 +8,11 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/pborman/getopt"
 )
-
-func Read(conn net.Conn) (string, error) {
-	reader := bufio.NewReader(conn)
-	var buffer bytes.Buffer
-	for {
-		ba, isPrefix, err := reader.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return "", err
-		}
-		buffer.Write(ba)
-		if !isPrefix {
-			break
-		}
-	}
-	return buffer.String(), nil
-}
 
 func main() {
 	// https://cloudflare-assignment.rahulnakre.workers.dev/
@@ -44,52 +26,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	// tcpAddr, err := net.ResolveTCPAddr("tcp4", "www.google.com")
-	// errHandler(err)
-
-	// fmt.Printf("here: %v", tcpAddr)
-
-	// conn, err := net.Dial("tcp", *urlFlag)
-	// errHandler(err)
-	// defer conn.Close()
-
-	// _, err = conn.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
-	// errHandler(err)
-
-	// res, err := ioutil.ReadAll(conn)
-	// errHandler(err)
-
-	// fmt.Println(string(res))
-
-	// s := "http://ran-home.s3-website-us-east-1.amazonaws.com"
-
 	u, err := url.Parse(*urlFlag)
 	checkError(err)
 	fmt.Println(u.Host)
 
-	// startTime := time.Now()
-	// // conn, err := net.Dial("tcp", "cloudflare-assignment.rahulnakre.workers.dev:http")
-	// conn, err := net.Dial("tcp", u.Host+":http")
-	// // conn, err := net.Dial("tcp", "www.google.com:http")
-	// // conn, err := net.Dial("tcp", "ran-home.s3-website-us-east-1.amazonaws.com:http")
+	// "http://cloudflare-assignment.rahulnakre.workers.dev"
+	// "http://www.google.com"
+	// "http://ran-home.s3-website-us-east-1.amazonaws.com"
 
-	// checkError(err)
-	// defer conn.Close()
-	// size, err := fmt.Fprintf(conn, "GET / HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n", u.Host)
-	// checkError(err)
-	// fmt.Println(size)
-
-	// res, err := ioutil.ReadAll(conn)
-	// endTime := time.Now()
-	// checkError(err)
-
-	// fmt.Printf(string(res) + "\n")
-	// fmt.Printf("%f\n", endTime.Sub(startTime).Seconds())
-
-	// fmt.Println(*profileFlag)
 	var slowestResTime, fastestResTime float64 = -1, -1
 	var totalTime float64 = 0
 	var successCount int64 = 0
+	// var totalBytesRead int =
 	for i := int64(0); i < *profileFlag; i++ {
 		startTime := time.Now()
 
@@ -97,19 +45,23 @@ func main() {
 		checkError(err)
 		defer conn.Close()
 
-		_, err = fmt.Fprintf(conn, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", u.Host)
+		// _, err = fmt.Fprintf(conn, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", u.Host)
+		_, err = fmt.Fprintf(conn, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", u.Path, u.Host)
 		checkError(err)
 
-		status, err := bufio.NewReader(conn).ReadString('\n')
-		// rest, err := bufio.NewReader(conn).ReadBytes()
-		s, err := Read(conn)
-		checkError(err)
+		// status, err := bufio.NewReader(conn).ReadString('\n')
+		// checkError(err)
+		// fmt.Printf(status)
+
+		_, totalBytesRead, err := Read(conn)
 		// res, err := ioutil.ReadAll(conn)
-		endTime := time.Now()
 		checkError(err)
 
-		fmt.Printf(status)
-		fmt.Printf(s)
+		endTime := time.Now()
+
+		// fmt.Printf(s + "\n")
+		fmt.Printf("bytes read: %d\n", totalBytesRead)
+		// fmt.Println(string(res) + "\n")
 
 		// fmt.Printf("size %d\n", len(res))
 		// if strings.Contains(status, "200") {
@@ -124,9 +76,6 @@ func main() {
 			slowestResTime = resTime
 		}
 		totalTime += resTime
-		// fmt.Printf(string(res) + "\n")
-		// fmt.Printf("%f\n", endTime.Sub(startTime).Seconds())
-
 	}
 
 	fmt.Printf("Number of requests: %d\n", *profileFlag)
@@ -136,6 +85,67 @@ func main() {
 	fmt.Printf("Median Response Time: %f\n", 1.0)
 	fmt.Printf("Percentage of Successful Requests: %f%%\n", float64(successCount)/float64(*profileFlag)*100)
 	fmt.Printf("")
+	fmt.Println(u)
+
+}
+
+// Read reads from a connection
+func Read(conn net.Conn) (string, int, error) {
+	reader := bufio.NewReader(conn)
+	// scanner := bufio.NewScanner(reader)
+	var buffer bytes.Buffer
+	var i int = -1
+	var totalBytesRead int = 0
+
+	// for scanner.Scan() {
+	// 	// fmt.Println("here")
+	// 	w := scanner.Text()
+	// 	buffer.WriteString(w)
+	// 	fmt.Println(w)
+	// 	fmt.Printf("%v\n", strings.Contains(w, "\n"))
+	// 	if len(w) >= 4 && w[len(w)-4:] == "\r\n\r\n" {
+	// 		buffer.WriteString("\n")
+	// 		fmt.Printf(buffer.String())
+	// 		buffer.Reset()
+	// 	}
+	// }
+
+	// return "sup", 3, nil
+	var isPrevDelim bool = false
+	for {
+		i++
+		// bytesArr, _, err := reader.ReadLine()
+		bytesArr, err := reader.ReadBytes('\n')
+		// fmt.Println(i)
+		// fmt.Println("ere")
+
+		fmt.Printf(string(bytesArr))
+		// fmt.Println(strings.Contains(string(bytesArr), "\r\n"))
+		// fmt.Println(strings.EqualFold(string(bytesArr), "\r\n"))
+
+		if strings.EqualFold(string(bytesArr), "\r\n") && isPrevDelim {
+			fmt.Println("END OF HEADER")
+		}
+
+		if strings.Contains(string(bytesArr), "\r\n") {
+			isPrevDelim = true
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("EOF")
+				break
+			}
+			return "", totalBytesRead, err
+		}
+		buffer.Write(bytesArr)
+		totalBytesRead += len(bytesArr)
+		// if !isPrefix {
+		// 	fmt.Println("breakubg cus isPrefix")
+		// 	break
+		// }
+	}
+	return buffer.String(), totalBytesRead, nil
 }
 
 func checkError(err error) {
